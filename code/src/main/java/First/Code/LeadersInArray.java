18d0bed0a6f34a41d76cdb9e28853c95a488a6ab
Tanushree Sarkar <tsarkar@altimetrik.com>
2024-02-08 15:47:30 +0530

    Leaders in an Array

---
+++ code/src/main/java/First/Code/LeadersInArray.java
@@ -0,0 +1,39 @@
+package First.Code;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeadersInArray {
+
+	static void findLeaders(int ar[], int n) {
+		
+		List<Integer> leaders = new ArrayList<Integer>();
+		
+		if (n == 0)
+			System.out.println("Empty array");
+
+		if (n == 1)
+			System.out.println(ar[n]);
+
+		int max = ar[n - 1];
+		 leaders.add(max);
+		for (int i = n - 2; i >= 0; i--) {
+			if (ar[i] > max) {
+				leaders.add(ar[i]);
+				max = ar[i];
+			}
+		}
+		
+		for(int i=leaders.size()-1;i>=0;i--)
+			System.out.print(leaders.get(i)+ " ");
+
+	}
+
+	public static void main(String args[]) {
+		int ar[] = { 16, 17, 4, 3, 5, 2 };
+		int ar1[] = {1,2,3,4,0};
+		findLeaders(ar, ar.length);
+		System.out.println();
+		findLeaders(ar1,ar1.length);
+	}
+}
