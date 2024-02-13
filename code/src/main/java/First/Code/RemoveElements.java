819094b428d08a0a84f1c9af149cb3f17f7618eb
Tanushree Sarkar <tsarkar@altimetrik.com>
2024-02-13 15:49:19 +0530

    changes for alignment

--- code/src/main/java/First/Code/RemoveElements.java
+++ code/src/main/java/First/Code/RemoveElements.java
@@ -5,25 +5,26 @@ public class RemoveElements {
 	public static void main(String args[]) {
 		int ar[] = { 10, 20, 20, 30 };
 		int newLength = removeDuplicates(ar);
-		
+
 		System.out.print("Array after removing duplicates: ");
-        for (int i = 0; i < newLength; i++) {
-            System.out.print(ar[i] + " ");
-        }
+		for (int i = 0; i < newLength; i++) {
+			System.out.print(ar[i] + " ");
+		}
 	}
 
 	public static int removeDuplicates(int[] nums) {
-        if (nums.length == 0) return 0;
-        
-        int uniqueCount = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1]) {
-                nums[uniqueCount] = nums[i];
-                uniqueCount++;
-            }
-        }
-        
-        return uniqueCount;
-    }
+		if (nums.length == 0)
+			return 0;
+
+		int uniqueCount = 1;
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] != nums[i - 1]) {
+				nums[uniqueCount] = nums[i];
+				uniqueCount++;
+			}
+		}
+
+		return uniqueCount;
+	}
 
 }
