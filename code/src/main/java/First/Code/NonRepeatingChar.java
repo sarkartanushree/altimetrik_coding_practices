cd8f2dcb4148f787069a3df91fabbd9d3b0a3b3f
Tanushree Sarkar <tsarkar@altimetrik.com>
2024-02-13 15:24:44 +0530

    changes for non repeating character

--- code/src/main/java/First/Code/NonRepeatingChar.java
+++ code/src/main/java/First/Code/NonRepeatingChar.java
@@ -1,14 +1,14 @@
 package First.Code;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class NonRepeatingChar {
 
 	public static void main(String args[]) {
 
-		String str = "Java developers are jool Dudes".toLowerCase();
-		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		String str = "Java developers are jool Dudes";
+		Map<Character, Integer> map = new LinkedHashMap<Character, Integer>();
 
 		for (int i = 0; i < str.length(); i++) {
 			if (!map.containsKey(str.charAt(i))) {
