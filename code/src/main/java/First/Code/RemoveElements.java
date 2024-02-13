245d2041114ed1c29de35e466b7cc040cfc7c605
Tanushree Sarkar <tsarkar@altimetrik.com>
2024-02-13 15:46:05 +0530

    optimize solution for removing duplicates

--- code/src/main/java/First/Code/RemoveElements.java
+++ code/src/main/java/First/Code/RemoveElements.java
@@ -1,28 +1,29 @@
 package First.Code;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 public class RemoveElements {
 
 	public static void main(String args[]) {
 		int ar[] = { 10, 20, 20, 30 };
-		removeElements(ar, ar.length);
+		int newLength = removeDuplicates(ar);
+		
+		System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(ar[i] + " ");
+        }
 	}
 
-	private static void removeElements(int[] ar, int n) {
-
-		Map<Integer, Integer> map = new LinkedHashMap<Integer, Integer>();
-		for (int i = 0; i < n; i++) {
-			if (map.containsValue(ar[i])) {
-				map.remove(i);
-			} else
-				map.put(i, ar[i]);
-		}
-
-		for (Map.Entry<Integer, Integer> num : map.entrySet()) {
-			System.out.print(num.getValue() + " ");
-		}
-	}
+	public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        
+        int uniqueCount = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[uniqueCount] = nums[i];
+                uniqueCount++;
+            }
+        }
+        
+        return uniqueCount;
+    }
 
 }
